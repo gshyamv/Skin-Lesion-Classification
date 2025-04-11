c4b64100148b78f14363437378652e87f2a20977
DevaRajan8 <rdeva6046@gmail.com>
2025-04-11 23:43:38 +0530

    cd

--- RN/LLM/AI_Doctor/format_summary.py
+++
@@ -1,6 +0,0 @@
-# Function to replace '\n' with '<br>'
-def replace_newline_with_br(input_string):
-    return input_string.replace('\n', '\n\n')
-
-def replace_t_with_tab(input_string):
-    return input_string.replace('\t', '    ')
--- RN/Utils/config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-export const config = {
-    mongoUri: 'mongodb://localhost:27017/imagedb'  // Add your database name
-  };
